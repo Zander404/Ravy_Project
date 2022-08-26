0dcdf5bcc5afab03c46739d0992c2e5a96a4607c
Zander404 <alexandrearthurpg@gmail.com>
2022-08-26 19:34:06 -0300

    Dockerfile

--- src/app/app/urls.py
+++ src/app/app/urls.py
@@ -19,5 +19,5 @@ from django.conf.urls import include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("souls/", include("souls_blog.urls")),
+    path("", include("souls_blog.urls")),
 ]
--- src/app/souls_blog/views.py
+++ src/app/souls_blog/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 
 # Create your views here.
 def home(request):
-    return render(request, "souls_blog/home.html")
+    return render(request, "souls_blog/erro404.html")
