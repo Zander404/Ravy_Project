e5f62c871723156a081de033cfff0ad29133df4c
zander404 <alexandrearthurpg@gmail.com>
2024-08-20 11:06:43 -0300

    startando tele

---
+++ blog/forms.py
@@ -0,0 +1,47 @@
+from models import User, Post, Role, Category, User_Role, Post_Category
+from django import forms
+
+
+class UserForm(forms.ModelForm):
+
+    class Meta:
+        model = User
+        fields = '__all__'
+
+
+
+class PostForm(forms.ModelForm):
+
+    class Meta:
+        model = Post
+        fields = '__all__'
+
+
+
+class RoleForm(forms.ModelForm):
+
+    class Meta:
+        model = Role
+        fields = '__all__'
+
+
+class CategoryForm(forms.ModelForm):
+
+    class Meta:
+        model = Category
+        fields = '__all__'
+
+
+class User_Role(forms.ModelForm):
+
+    class Meta:
+        model = User_Role
+        fields = '__all__'
+
+
+class Post_Category(forms.ModelForm):
+
+    class Meta:
+        model = Post_Category
+        fields = '__all__'
+
