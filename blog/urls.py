d5d416e23aea5522233afa5d208d5e38a6f02504
zander404 <alexandrearthurpg@gmail.com>
2024-08-21 22:31:24 -0300

    fazendo template

--- blog/urls.py
+++ blog/urls.py
@@ -2,5 +2,5 @@ from django.urls import path, include
 from blog import views
 
 urlpatterns = [
-    path('', )
+    path('', views.index)
 ]
---
+++ static/js/taiwinds_conf.js
@@ -0,0 +1,15 @@
+tailwind.config = {
+    theme: {
+        extend: {
+            colors: {
+                dark_primary: '#8D00DE',
+                dark_secondary: '#8A6ECD',
+                dark_accent_primary: '#D1C4E9',
+                dark_accent_secondary: '#D1C4E9',
+
+
+                white_acent_secondary: '#6200EA'
+            }
+        }
+    }
+}
