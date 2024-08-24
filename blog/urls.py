973662d189b375cda6d7b97686e513954dc2355d
zander404 <alexandrearthurpg@gmail.com>
2024-08-24 15:21:47 -0300

    add read_more

--- blog/urls.py
+++ blog/urls.py
@@ -1,6 +1,12 @@
+from django.conf import settings
+from django.conf.urls.static import static
 from django.urls import path, include
 from blog import views
 
 urlpatterns = [
-    path('', views.index)
+    path('', views.index),
+    path('poema/<int:pk>', views.read_more, name="poema")
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
